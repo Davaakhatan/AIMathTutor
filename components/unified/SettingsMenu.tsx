da5cb64971d17b91436fb7954ea0ab72720d020f
Dave <zr.davaa@gmail.com>
2025-11-06 22:55:31 -0600

    Remove invalid scrollbar classes, use existing CSS scrollbar styling

--- components/unified/SettingsMenu.tsx
+++ components/unified/SettingsMenu.tsx
@@ -204,7 +204,7 @@ export default function SettingsMenu({ onXPDataChange }: SettingsMenuProps) {
       </div>
 
       {/* Tab Content */}
-      <div className="flex-1 overflow-y-auto min-h-0 overscroll-contain scrollbar-thin scrollbar-thumb-gray-300 dark:scrollbar-thumb-gray-600 scrollbar-track-transparent" style={{ maxHeight: 'calc(85vh - 80px)' }}>
+      <div className="flex-1 overflow-y-auto min-h-0 overscroll-contain" style={{ maxHeight: 'calc(85vh - 80px)' }}>
         {activeTab === "settings" && <SettingsContent />}
         {activeTab === "notifications" && <NotificationsContent />}
         {activeTab === "xp" && <XPContent onXPDataChange={onXPDataChange} />}
